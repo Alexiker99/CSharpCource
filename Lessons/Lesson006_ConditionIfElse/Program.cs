dfa9f9a92ce0d1b85e665a14a3ce9abac7d75673
Alexiker99 <alexiker99@gmail.com>
2022-11-30 12:37:48 +0400

    add ConditionIfElse

---
+++ Lessons/Lesson006_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "юля")
+{
+    Console.WriteLine("Ура, это же Юля!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
