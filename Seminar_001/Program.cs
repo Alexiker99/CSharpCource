cb4809b47405b7ab6b3dc9f01a37ed392d246824
Alexiker99 <alexiker99@gmail.com>
2022-12-01 16:30:07 +0400

    Add Ex_001_sqrtnumb

---
+++ Seminar_001/Program.cs
@@ -0,0 +1,7 @@
+﻿//возведение числа в квадрат//
+Console.WriteLine("Start");
+
+int number = 25;  // Определили целое число
+int result = number * number; // Вычисление квадрата числа
+Console.WriteLine(result); // Выводим результат
+Console.WriteLine("end");
