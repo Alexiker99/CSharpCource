d4b586f90be0e2f2a78e7c3c367ff3e1abd7e943
Alexiker99 <alexiker99@gmail.com>
2022-12-01 21:43:53 +0400

    add Ex_001_FindMaxwith3numbers

---
+++ Home_work_001/Ex_001_FindMaxwith3numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Start");
+
+int a = 4;
+int b = 3;
+int c = 22;
+int max = 0;
+
+max = a;
+if (b > max){
+    max = b;
+    };
+if (c > max){
+    max = c;
+    }
+
+
+
+System.Console.WriteLine(max);
+Console.WriteLine("End");
