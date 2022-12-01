67131bd09a28f44d035a71263742bf79b80253ec
Alexiker99 <alexiker99@gmail.com>
2022-12-01 16:41:27 +0400

    Fix problem

---
+++ Seminar_001/Ex_001_sqrtnumb/Program.cs
@@ -0,0 +1,9 @@
+﻿//возведение числа в квадрат//
+using System;
+
+Console.WriteLine("Start");
+
+int number = 25;  // Определили целое число
+int result = number * number; // Вычисление квадрата числа
+Console.WriteLine(result); // Выводим результат
+Console.WriteLine("end");
