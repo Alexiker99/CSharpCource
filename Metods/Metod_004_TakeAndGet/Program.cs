4f7bc31f5c331cac4f818857a88f51492fcb4675
Alexiker99 <alexiker99@gmail.com>
2022-12-09 14:31:28 +0400

    add Metod_004

---
+++ Metods/Metod_004_TakeAndGet/Program.cs
@@ -0,0 +1,18 @@
+﻿// Самый главный и частый метод, который принимает на входе и дает на выходе.
+// Пример 
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+;
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+
+}
+
+string result = Method4(10, "z");
+Console.WriteLine(result);
