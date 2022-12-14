59263fb1bda14b4922fb756a80b1f4fdd86d6a65
Alexiker99 <alexiker99@gmail.com>
2022-12-14 21:07:41 +0400

    add Matrix

---
+++ Lessons/Lesson016_Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿int [,] matrix = new int [3, 4]; // вывести матрицу [3,4]
+ void PrintArray(int[,] matr)
+{
+
+ for(int i = 0; i < matr.GetLength(0); i++) //Колличество строк
+ {
+    for(int j = 0; j < matr.GetLength(1); j++) // Колличество столбцов 
+    {
+        Console.Write($"{matr[i, j]} "); //Вывод данных
+    }
+        
+  Console.WriteLine();
+ }
+}
+
+void FillArray(int[,] matr)
+{
+
+ for(int i = 0; i < matr.GetLength(0); i++) //Колличество строк
+ {
+    for(int j = 0; j < matr.GetLength(1); j++) // Колличество столбцов 
+    {
+        matr[i,j] = new Random().Next(1,10);// Полуинтервал [1;10). Обращаемся к конкретной позиции 
+    }
+
+ }
+
+}
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
