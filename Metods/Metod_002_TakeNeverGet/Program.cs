fd57ba74b88be1bc4c2ad662eb7c7e797e071b90
Alexiker99 <alexiker99@gmail.com>
2022-12-14 10:45:43 +0400

    Update code

--- Metods/Metod_002_TakeNeverGet/Program.cs
+++ Metods/Metod_002_TakeNeverGet/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Метод который принимает , но не возвращает 
 // Пример:
 
-void Method2(string msg, int count);
+void Method2(string msg, int count)
 {
     int i = 0;
     while (i < count)
     {
-        System.Console.WriteLine(msg);
+        System.Console.Write(msg);
         i++; //Инкримент
     }
 }
