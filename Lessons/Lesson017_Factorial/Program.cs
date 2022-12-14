9658d3ef6b6acd329ec72c227afcb6be8d40a6f4
Alexiker99 <alexiker99@gmail.com>
2022-12-14 22:00:10 +0400

    add Lesson017

---
+++ Lessons/Lesson017_Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿//Нахождение факотриала с помошью Рекурсии.
+
+double Factorial (int n)
+{
+    // 1! = 1                                    5! = 5*!4
+    // 0! = 1                                            4*!3
+    if(n == 1) return 1;                  //                3*!2
+    else return n * Factorial (n - 1);     //                  2*!1
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
