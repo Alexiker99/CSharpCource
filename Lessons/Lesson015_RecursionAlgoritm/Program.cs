dc20c7c846c78f6e74254800930e648e4a0340f8
Alexiker99 <alexiker99@gmail.com>
2022-12-14 20:27:30 +0400

    add lesson 015

---
+++ Lessons/Lesson015_RecursionAlgoritm/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string[,] table = new string [2, 5];
+//String.Emty
+//table [0,0] table[0,1] table[0, 2] table[0, 4]
+//table [1,0] table[1,1] table [1, 2] table [1,4]
+table[1, 2] = "Привет";
+// Воспользуемся циклом в цикле
+
+for(int rows = 0; rows < 2; rows++) //Колличество строк
+{
+    for(int columns = 0; columns < 5; columns++) // Колличество столбцов 
+    {
+        Console.WriteLine($"-{table[rows, columns]}-"); //Вывод данных
+    }
+        
+}
