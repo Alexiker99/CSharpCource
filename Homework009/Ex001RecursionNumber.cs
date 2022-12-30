b668c13bf8cb01dc04e57dc048c4d614a443490c
Alexiker99 <alexiker99@gmail.com>
2022-12-30 11:50:41 +0400

    add Ex001

---
+++ Homework009/Ex001RecursionNumber.cs
@@ -0,0 +1,20 @@
+using static Methods;
+public class Ex001RecursionNumber
+{
+    static public void Task64()
+    // Задайте значение N. Напишите программу, 
+    // которая выведет все натуральные числа в промежутке от N до 1. 
+    // Выполнить с помощью рекурсии.
+    // N = 5 -> "5, 4, 3, 2, 1"
+    // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+    {
+        int n = InputInt("Введите положительное число: ");
+        int m = 1;
+        if (n < 1)
+        {
+            Console.WriteLine("Ввели не положительное число");
+        }
+        Console.WriteLine(NaturalNumber(n, m));
+    }
+
+}
